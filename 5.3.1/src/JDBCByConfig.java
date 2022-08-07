618c7548fb381787b867f8f25a4f3282016c41df
lidaren9 <lidaren_9@163.com>
2022-08-07 10:13:25 +0800

    将相对固定的连接信息写入到配置文件中更新

--- 5.3.1/src/JDBCByConfig.java
+++ 5.3.1/src/JDBCByConfig.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.sql.*;
 import java.util.Properties;
 
 public class JDBCByConfig {
@@ -16,8 +17,9 @@ public class JDBCByConfig {
         Properties prop = new Properties();
         InputStream in = null;
         try {
+            System.out.println(fileName);
             // 读配置和文件
-            in = new BufferedInputStream(new FileInputStream("src/prop/" + fileName));
+            in = new BufferedInputStream(new FileInputStream("5.3.1/src/prop/" + fileName));
             prop.load(in);
             // 读JDBC连接参数
             driver = prop.getProperty("mysql.Driver");
@@ -26,6 +28,7 @@ public class JDBCByConfig {
             pwd = prop.getProperty("mysql.pwd");
         } catch (IOException e) {
             e.printStackTrace();
+            return;
         } finally {
             if (in != null) {
                 try {
@@ -40,6 +43,27 @@ public class JDBCByConfig {
             Class.forName(driver);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
+        }
+        Connection connection = null;
+        PreparedStatement ps = null;
+        try {
+            connection = DriverManager.getConnection(url, username, pwd);
+            ps = connection.prepareStatement("select id, name, score from student");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                System.out.print("id : " + rs.getString("id")  + " ");
+                System.out.print("name : " + rs.getString("name") + " ");
+                System.out.println("Score : " + rs.getInt("score"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                ps.close();
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
